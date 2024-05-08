d15fe14d00036a36c2add8d2144fe38c0bd1a515
dianadianaxd <159160020+dianadianaxd@users.noreply.github.com>
2024-05-07 19:39:06 -0600

    Create drawer_menu.dart

---
+++ lib/drawer_menu.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class DrawerMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          _buildDrawerHeader(),
+          _buildDrawerItem(
+              icon: Icons.home,
+              text: 'Fila centrada',
+              onTap: () =>
+                  {Navigator.pushReplacementNamed(context, MyApp.home)}),
+          _buildDrawerItem(
+              icon: Icons.account_circle,
+              text: 'Fila incio',
+              onTap: () =>
+                  {Navigator.pushReplacementNamed(context, MyApp.profile)}),
+          _buildDrawerItem(
+              icon: Icons.movie,
+              text: 'Fila final',
+              onTap: () =>
+                  {Navigator.pushReplacementNamed(context, MyApp.movies)}),
+          Divider(),
+          _buildDrawerItem(
+              icon: Icons.contact_phone,
+              text: 'Fila espaciada',
+              onTap: () =>
+                  {Navigator.pushReplacementNamed(context, MyApp.contacts)}),
+          _buildDrawerItem(
+              icon: Icons.space_bar,
+              text: 'Fila espaciada alrededor',
+              onTap: () =>
+                  {Navigator.pushReplacementNamed(context, MyApp.pag5)}),
+          _buildDrawerItem(
+              icon: Icons.space_dashboard,
+              text: 'Fila entre espaciada',
+              onTap: () =>
+                  {Navigator.pushReplacementNamed(context, MyApp.pag6)}),
+          _buildDrawerItem(
+              icon: Icons.collections,
+              text: 'Fila estirada',
+              onTap: () =>
+                  {Navigator.pushReplacementNamed(context, MyApp.pag7)}),
+          ListTile(
+            title: Text('App version 1.0.0'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerHeader() {
+    return DrawerHeader(
+        margin: EdgeInsets.zero,
+        padding: EdgeInsets.zero,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage('assets/images/avatar1.jpg'))),
+        child: Stack(children: <Widget>[
+          Positioned(
+              bottom: 12.0,
+              left: 16.0,
+              child: Text("Compilación Movil",
+                  style: TextStyle(
+                      color: Color(0xff000000),
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500))),
+        ]));
+  }
+
+  Widget _buildDrawerItem(
+      {required IconData icon,
+      required String text,
+      required GestureTapCallback onTap}) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Icon(icon),
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Text(text),
+          )
+        ],
+      ),
+      onTap: onTap,
+    );
+  }
+}
